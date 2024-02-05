b62d3da48941b0fd42f2f857f664afe82b885332
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2024-02-05 13:50:17 +0700

    create route for producer

--- src/controller/router.js
+++ src/controller/router.js
@@ -1,15 +1,19 @@
 
 
-async function router(fastify,options){
-    fastify.get('/', async function handler (request, reply) {
+const router = async (fastifyInstance,options,next)=> {
+    fastifyInstance.get('/', async function handler (request, reply) {
         return { hello: 'world' }
       });
-    fastify.get('/produce',async function handler(request,reply){
-        
-        return {message: 'message sent'}
-    })
-    
 
+    fastifyInstance.get('/producer', async function handler (request, reply) {
+        fastifyInstance.kafka.producer.send({
+            topic: 'test-topic',
+            messages: [{ key: 'key1', value: "hello" }]
+        });
+        return { status: 'ok' }
+    });
+    
+next();
      
 }
 

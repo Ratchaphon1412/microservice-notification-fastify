0849e3516860ef447e695104c7e6d454113ab38a
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2024-02-05 15:50:38 +0700

    add config mailer

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ import Fastify from 'fastify';
 import fastifyKafkaJS from 'fastify-kafkajs'
 import fastifyGracefulShutdown from "fastify-graceful-shutdown"
 import {configKafka} from './src/utils/kafka/config.js'
+
+import fastify_nodemailer from "fastify-nodemailer"
+import {mailConfig} from './src/utils/notification/config.js'
 import router from './src/controller/router.js'
 
 
@@ -11,13 +14,16 @@ const fastify = Fastify({ logger: true })
 
 fastify.register(fastifyKafkaJS,configKafka)
 fastify.register(fastifyGracefulShutdown) 
+fastify.register(fastify_nodemailer,mailConfig )
 fastify.register(router)
   
 
 
 const start = async () => {
     try {
+      
       await fastify.listen({ port: PORT, host: ADDRESS })
+      
     } catch (err) {
       fastify.log.error(err)
       process.exit(1)
